885307ee68e1ebe9ce552208df4d314c3e12bee0
Yurlian <yurliansivira@gmail.com>
2025-10-13 06:56:41 -0300

    modificacion de archivo

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 // Genera un token JWT
 const generateToken = (id, rol) => {
     return jwt.sign({ id, rol }, process.env.JWT_SECRET, {
-        expiresIn: '1h', // Puedes ajustar el tiempo de expiración
+        expiresIn: '1h', // El token expira en 1 hora
     });
 };
 
